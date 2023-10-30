5caa9dde96df020602872ec60f06c53b1c94213f
ADS 2022.2N <36124872022.2N@senacrjedu.local>
2023-10-30 19:24:14 -0300

    novo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { EnderecoComponent } from './endereco/endereco.component';
+import { ColetaComponent } from './coleta/coleta.component';
+import { FotoperfilComponent } from './fotoperfil/fotoperfil.component';
+import { CargaComponent } from './carga/carga.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerfilComponent,
+    EnderecoComponent,
+    ColetaComponent,
+    FotoperfilComponent,
+    CargaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/coleta/coleta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coleta',
+  templateUrl: './coleta.component.html',
+  styleUrls: ['./coleta.component.css']
+})
+export class ColetaComponent {
+
+}
